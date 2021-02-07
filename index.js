6e9a4ab85eb5ca2d5a9a3b1b3c2f4fccfd322c1b
swade781977 <swade781977@yahoo.com>
2021-02-06 19:30:37 -0500

    commit tree 1571c7bf90fda08fa39b7131416bd62df91ddfdd

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(flair="*"){
+    return function inner(arg="special"){
+        return `You are ${flair}${arg}${flair}!`
+    }
+}
+
+let  Calculator = {
+    add: function(a,b){
+        return (a + b)
+    },
+    subtract: function(a,b){
+        return (a - b)
+    },
+    multiply: function(a,b){
+        return (a * b)
+    },
+    divide: function(a, b){
+        return (a / b)
+    }
+}
+
+function actionApplyer(num, array){
+    let a = num
+    for(let i=0; i < array.length; i++){
+        a = array[i](a)
+    }
+    return a
+}
